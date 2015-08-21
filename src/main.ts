e019d2bb580f3d7ae13fd3c51801c6c851612d14
Mehuge <adf@sorcerer.co.uk>
2015-08-21 23:01:48 +0100

    Lets get some events flying!

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 /// <reference path="definitions/tsd.d.ts" />
 import * as core from 'cu-core';
-import * as events from 'cu-events';
-console.dir(core);
-console.dir(events);
-debugger;
+import events from 'cu-events';
+const characterEvent = events.on("character", (data: any) => {
+    console.dir('character data ' + JSON.stringify(data));
+});
+const enemyTargetEvent = events.on("enemytarget", (data: any) => {
+    console.dir('enemytarget data ' + JSON.stringify(data));
+});
+const friendlyTargetEvent = events.on("friendlytarget", (data: any) => {
+    console.dir('friendlytarget data ' + JSON.stringify(data));
+});
