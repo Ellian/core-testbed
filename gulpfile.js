ab94d4267647420f8f04619fa97304a9ac193f08
Mehuge <adf@sorcerer.co.uk>
2015-08-23 23:41:46 +0100

    Override copy:assets to include resources from cu-components that we need.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,38 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
- 'use strict';
+'use strict';
 
- var gulp = require('gulp');
- var buildConfig = require('./cu-build.config.js');
- var buildTools = require('cu-build-tools');
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var buildConfig = require('./cu-build.config.js');
+var buildTools = require('cu-build-tools');
 
 buildTools.auto(gulp, buildConfig);
+
+gulp.task('copy-images', function () {
+  console.log('copy images');
+  gulp.src('node_modules/cu-components/lib/classes/unitframe/images/*')
+    .pipe(debug({ title: 'source:' }))
+    .pipe(gulp.dest('dist/cu-components/unitframe/images'))
+    .pipe(debug({ title: 'output:' }));
+});
+
+gulp.task('copy-css', function () {
+  console.log('copy css');
+  gulp.src('node_modules/cu-components/lib/classes/unitframe.css')
+    .pipe(debug({ title: 'source:' }))
+    .pipe(gulp.dest('dist/cu-components'))
+    .pipe(debug({ title: 'output:' }));
+});
+
+gulp.task('copy-html', function () {
+  console.log('copy css');
+  gulp.src('src/**.html')
+    .pipe(debug({ title: 'source:' }))
+    .pipe(gulp.dest('dist'))
+    .pipe(debug({ title: 'output:' }));
+});
+
+gulp.task('copy:assets', [ 'copy-html', 'copy-images', 'copy-css' ]);
+
