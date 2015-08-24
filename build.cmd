142cb7a849b687395fcd059054acb26e2b0d8742
Mehuge <adf@sorcerer.co.uk>
2015-08-24 18:37:48 +0100

    build fake API

--- build.cmd
+++ build.cmd
@@ -1,12 +1,19 @@
 cd ../libs/cu-events
+call gulp clean
 call gulp
 
 cd ../cu-stores
+call gulp clean
 call gulp
 
 cd ../cu-components
+call gulp clean
+call gulp
+
+cd ../cu-fake-api
+call gulp clean
 call gulp
 
 cd ../../core-testbed
+call gulp clean
 call gulp
-copy src\fake*.js dist\js
